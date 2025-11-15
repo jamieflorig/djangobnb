7713a0d5b8bd3869ac519d323068d8a5c85b8cd3
jamieflorig <jamie.florig@msunaawan.edu.ph>
2025-11-15 23:40:40 +0800

    Video 2-Landlord page - Florig, Jamie D.

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 const Categories = () => {
     return (
-        <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <div className="pt-6 cursor-pointer pb-6 flex items-center space-x-12">
+            <div className="pb-6 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
                 <Image
                     src="/icn_category_beach.jpeg"
                     alt="Category - Beach"
@@ -14,7 +14,7 @@ const Categories = () => {
                 <span className='text-xs'>Beach</span>
             </div>
 
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+            <div className="pb-6 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
                 <Image
                     src="/icn_category_beach.jpeg"
                     alt="Category - Beach"
@@ -25,7 +25,7 @@ const Categories = () => {
                 <span className='text-xs'>Villas</span>
             </div>
 
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+            <div className="pb-6 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
                 <Image
                     src="/icn_category_beach.jpeg"
                     alt="Category - Beach"
@@ -36,7 +36,7 @@ const Categories = () => {
                 <span className='text-xs'>Cabins</span>
             </div>
 
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+            <div className="pb-6 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
                 <Image
                     src="/icn_category_beach.jpeg"
                     alt="Category - Beach"
