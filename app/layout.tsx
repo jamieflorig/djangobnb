e9da29f9bc894647b5bb7fb926d9faa83b194cfa
jamieflorig <jamie.florig@msunaawan.edu.ph>
2025-11-16 16:32:53 +0800

    Video 2-Log in modal - Florig, Jamie D.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Modal from "./components/modals/Modal";
+import LoginModal from "./components/modals/LoginModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,8 @@ export default function RootLayout({
           <div className="pt-24">
             {children}
           </div>
+
+          <LoginModal />
       </body>
     </html>
   );
